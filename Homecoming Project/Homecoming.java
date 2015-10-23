68b10f5c0fe29cfd1c68a723c4fe8798a3a57859
TommyLi <tommyli76278@gmail.com>
2015-10-23 08:53:55 -0700

    homecoming

---
+++ Homecoming Project/Homecoming.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Homecoming here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner;
+public class Homecoming
+{
+   Scanner console = new Scanner(System.in);
+   public static void main(String[] args){
+       getADate(); 
+    }
+    
+   public static void getADate(){
+        System.out.print("Hi! My name is ");
+        String myName = scanner.nextLine();
+   }
+}
