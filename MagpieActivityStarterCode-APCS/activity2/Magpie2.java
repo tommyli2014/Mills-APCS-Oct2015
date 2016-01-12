477b450ce61f0d173f07dba295552f55de713f2a
TommyLi <tommyli76278@gmail.com>
2016-01-12 09:30:13 -0800

    magpie

---
+++ MagpieActivityStarterCode-APCS/activity2/Magpie2.java
@@ -0,0 +1,115 @@
+package activity2;
+
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:  
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ *          Handles responding to simple words and phrases 
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie2
+{
+    /**
+     * Get a default greeting   
+     * @return a greeting
+     */
+    public String getGreeting()
+    {
+        return "Hello, let's talk.";
+    }
+    
+    /**
+     * Gives a response to a user statement
+     * 
+     * @param statement
+     *            the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement)
+    {
+        String response = "";
+        if (statement.indexOf("no") >= 0)
+        {
+            response = "Why so negative?";
+        }
+        else if (statement.indexOf("mother") >= 0
+                || statement.indexOf("father") >= 0
+                || statement.indexOf("sister") >= 0
+                || statement.indexOf("brother") >= 0)
+        {
+            response = "Tell me more about your family.";
+        }
+        else if (statement.indexOf("cat") >= 0
+                 || statement.indexOf("dog") >= 0
+                 || statement.indexOf("goldfish") >= 0)
+        {
+            response = "Tell me more about your pet.";
+        }
+        else if (statement.indexOf("Dreyer") >= 0) {
+            response = "She sounds like a greate teacher.";
+        }
+        else if (statement.trim().length() == 0){
+            response = "Say something, please.";
+        }
+        else if (statement.indexOf("damn") >= 0){
+            response = "Please be nice.";
+        }
+        else if (statement.indexOf("weather") >= 0){
+            response = "It's nice today.";
+        }
+        else
+        {
+            response = getRandomResponse();
+        }
+        return response;
+    }
+
+    /**
+     * Pick a default response to use if nothing else fits.
+     * @return a non-committal string
+     */
+    private String getRandomResponse()
+    {
+        final int NUMBER_OF_RESPONSES = 4;
+        double r = Math.random();
+        int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+        String response = "";
+        
+        if (whichResponse == 0)
+        {
+            response = "Interesting, tell me more.";
+        }
+        else if (whichResponse == 1)
+        {
+            response = "Hmmm.";
+        }
+        else if (whichResponse == 2)
+        {
+            response = "Do you really think so?";
+        }
+        else if (whichResponse == 3)
+        {
+            response = "You don't say.";
+        }
+        else if (whichResponse == 4)
+        {
+            response = "Giggty.";
+        }
+        else if (whichResponse == 5)
+        {
+            response = "Alright.";
+        }
+        else if (whichResponse == 6)
+        {
+            response = "How random is this response.";
+        }
+        
+
+        return response;
+    }
+}
